5b5ad4b8e6fe5209fc9a16f6c11e31dbb19c5201
Whitley Horn <whitleyblaine@protonmail.com>
2022-03-01 21:11:21 -0500

    Add mutation observer for lazy loaded images

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 console.log("test message");
 
-findImageSource(document.body);
+Array.from(document.querySelectorAll("img")).forEach(function observe(img) {
+  if (isImgSrc(img.src)) {
+    // Img src not waiting to be lazy loaded, so wrap immediately
+    wrap(img);
+  } else {
+    // Wait for img src to be lazy loaded before wrapping
+    var observer = new MutationObserver(function (changes) {
+      changes.forEach((change) => {
+        if (change.attributeName.includes("src")) wrap(img);
+      });
+    });
+    observer.observe(img, { attributes: true });
+  }
+});
+
+// ***
+
 function wrap(toWrap, wrapper) {
-    wrapper = wrapper || document.createElement("div");
-    toWrap.parentNode.appendChild(wrapper);
-    return wrapper.appendChild(toWrap);
+  wrapper = wrapper || document.createElement("div");
+  toWrap.parentNode.appendChild(wrapper);
+  toWrap.insertAdjacentHTML(
+    "beforebegin",
+    '<div id="backDate">Test Date</div>'
+  );
+  return wrapper.appendChild(toWrap);
 }
 
-function findImageSource(element) {
-    if (element.hasChildNodes()) {
-        element.childNodes.forEach(findImageSource);
-    } else if (element.nodeName == "IMG") {
-        console.log(element.src);
-        wrap(element);
-        element.parentNode.id = 'wrap'
-        element.insertAdjacentHTML(
-            "beforebegin",
-            '<div id="backDate">Test Date</div>'
-        );
-    }
+function isImgSrc(url) {
+  return url.match(/\.(jpeg|jpg|gif|png)$/) != null;
 }
