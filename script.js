355c1f8cc79399d668234aff8a8c8c18ec6f4539
Whitley Horn <whitleyblaine@protonmail.com>
2022-03-01 22:11:39 -0500

    Refactor

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 console.log("test message");
 
-Array.from(document.querySelectorAll("img")).forEach(function observe(img) {
+Array.from(document.querySelectorAll("img")).forEach(addDateDiv);
+
+// ***
+function addDateDiv(img) {
+  console.log("img", img);
   if (isImgSrc(img.src)) {
     // Img src not waiting to be lazy loaded, so wrap immediately
     wrap(img);
   } else {
     // Wait for img src to be lazy loaded before wrapping
-    var observer = new MutationObserver(function (changes) {
-      changes.forEach((change) => {
-        if (change.attributeName.includes("src")) wrap(img);
-      });
-    });
-    observer.observe(img, { attributes: true });
+    observeImg(img);
   }
-});
+}
 
-// ***
+function observeImg(img) {
+  const imgObserver = new MutationObserver(observerCallback);
+
+  imgObserver.observe(img, { attributes: true });
+
+  // ***
+  function observerCallback(changes, observer) {
+    const srcChange = changes.find((c) => c.attributeName.includes("src"));
+    if (!srcChange) return;
+    wrap(img);
+    // Disconnect observer because it has accomplished its objective and is a drag on performance
+    observer.disconnect();
+  }
+}
 
 function wrap(toWrap, wrapper) {
   wrapper = wrapper || document.createElement("div");
